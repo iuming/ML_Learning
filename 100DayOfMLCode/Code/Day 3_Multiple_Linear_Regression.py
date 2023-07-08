6a61b9faf394fc8bd27c4a06ee0745ddf4f49468
iuming <6ming@hrbeu.edu.cn>
2023-07-08 10:05:17 +0800

    update files

--- 100DayOfMLCode/Code/Day 3_Multiple_Linear_Regression.py
+++ 100DayOfMLCode/Code/Day 3_Multiple_Linear_Regression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 dataset = pd.read_csv('../datasets/50_Startups.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : ,  4 ].values
@@ -12,4 +13,10 @@ print("Y:")
 print(Y)
 print("Z:")
 print(Z)
-dataset.head(5)
+# dataset.head(5)
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[: , 3] = labelencoder.fit_transform(X[ : , 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
