8ad4589d15509d9c6d0a66606c9c9175638b7de1
mliu <mliu@ihep.ac.cn>
2024-11-19 14:26:46 +0800

    add example using GPU calculate

---
+++ RL_Learning/example_gpu.py
@@ -0,0 +1,25 @@
+# Program Name: example_gpu
+# Author: Ming Liu
+# Created Time: November, 9, 2024, 12:31 am
+
+import gymnasium as gym
+
+from stable_baselines3 import A2C
+
+env = gym.make("CartPole-v1", render_mode="rgb_array")
+
+model = A2C("MlpPolicy", env, verbose=1, device='cuda:0', tensorboard_log="./a2c_cartpole_tensorboard/")
+model.load("cartpole_a2c")
+# model.learn(total_timesteps=1000_000)
+# model.save("cartpole_a2c")
+
+vec_env = model.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = vec_env.step(action)
+    # print(reward)
+    vec_env.render("human")
+    # VecEnv resets automatically
+    # if done:
+    #   obs = vec_env.reset()
