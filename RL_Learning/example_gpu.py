48664c1490a3ca80218eef691bc66cbdc9b7b180
iuming <6ming@hrbeu.edu.cn>
2025-02-28 21:44:43 +0800

    添加GPU训练模型示例，更新example_gpu.py并启用模型学习与保存功能

--- RL_Learning/example_gpu.py
+++ RL_Learning/example_gpu.py
@@ -2,6 +2,10 @@
 # Author: Ming Liu
 # Created Time: November, 9, 2024, 12:31 am
 
+# Modified Time: February, 28, 2025, 20:07 pm
+# Modified Author: Ming Liu
+# Modified Notes: Add a new example of using GPU to train a model.
+
 import gymnasium as gym
 
 from stable_baselines3 import A2C
@@ -9,9 +13,9 @@ from stable_baselines3 import A2C
 env = gym.make("CartPole-v1", render_mode="rgb_array")
 
 model = A2C("MlpPolicy", env, verbose=1, device='cuda:0', tensorboard_log="./a2c_cartpole_tensorboard/")
-model.load("cartpole_a2c")
-# model.learn(total_timesteps=1000_000)
-# model.save("cartpole_a2c")
+# model.load("cartpole_a2c")
+model.learn(total_timesteps=1000_000)
+model.save("cartpole_a2c")
 
 vec_env = model.get_env()
 obs = vec_env.reset()
